f3898bb7b001c31a339e8f4a4f4d6ace2d3edb09
DUCLOS NOÉ <noe.duclos@viacesi.fr>
2021-06-30 14:55:24 +0200

    feat(swagger): adding swagger
    adding swagger and starting to write documentation so the people who want to stole our work can do
    it easyly

--- src/routes/tags.router.js
+++ src/routes/tags.router.js
@@ -2,7 +2,34 @@ const express = require('express');
 const tagController = require('../controllers/tagController');
 const router = express.Router();
 
-/* GET tag listing. */
+/**
+ * @swagger
+ * /tags:
+ *  get:
+ *    summary: Retrieve a list of JSON tag
+ *    description: Retrieve the complete list of tags descripting the stores for search purpose
+ *    responses:
+ *       200:
+ *         description: list of tags
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: tag's id
+ *                         example: 0
+ *                       name:
+ *                         type: string
+ *                         description: tag's name
+ *                         example: italien
+ */
 router.get('/', tagController.findAll);
 
 module.exports = router;
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -3,7 +3,34 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 const { convertImage } = require('../middlewares/images.middleware');
 
-/* GET users listing. */
+/**
+ * @swagger
+ * /tags:
+ *  get:
+ *    summary: Retrieve a list of JSON user
+ *    description: Retrieve the complete list of tags descripting the stores for search purpose
+ *    responses:
+ *       200:
+ *         description: list of tags
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: tag's id
+ *                         example: 0
+ *                       name:
+ *                         type: string
+ *                         description: tag's name
+ *                         example: italien
+ */
 router.get('/', userController.getAll);
 
 /* GET on user by it's ID */
